62ae208f5fc9bc655fd78c9e9b9d91042cbb153c
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-11-18 09:51:55 +0100

    feat: add get request for all cupcakes to api service

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Cupcake } from '../models/cupcake.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private http = inject(HttpClient);
+  private baseApiUrl = 'http://localhost:4000';
 
-  constructor() { }
+  getCupcakes(): Observable<Cupcake[]> {
+    return this.http.get<Cupcake[]>(`${this.baseApiUrl}/cupcakes`);
+  }
 }
