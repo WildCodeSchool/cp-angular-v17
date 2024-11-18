31e07126bf8aa5e7b284f0bc3cacfe11f61a63a2
jokay03J <magrojoan3@gmail.com>
2024-11-18 10:26:02 +0100

    Ajout des méthodes pour récupérer les cupcakes et les accessoires

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Cupcake } from '../models/cupcake.model';
+import { Accessory } from '../models/accessory.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  http = inject(HttpClient)
 
   constructor() { }
+
+  getCupcakes() {
+    return this.http.get<Cupcake[]>('http://localhost:4000/cupcakes');
+  }
+
+  getCupcake(id: string) {
+    return this.http.get<Cupcake>(`http://localhost:4000/cupcakes/${id}`);
+  }
+
+  getAccessories() {
+    return this.http.get<Accessory[]>('http://localhost:4000/accessories');
+  }
 }
