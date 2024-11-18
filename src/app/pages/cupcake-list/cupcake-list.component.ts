4626d660034e745fa97115c5a24f880df1f0e328
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-11-18 10:00:53 +0100

    Step 1 - Get the cupcakes

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
+import {Cupcake} from "../../models/cupcake.model";
+import {ApiService} from "../../shared/api.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-cupcake-list',
@@ -10,6 +13,18 @@ import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
 })
 export class CupcakeListComponent {
   // Step 1: get all cupcakes
+  cupcakes$!: Observable<Cupcake>;
+
+  private apiService: ApiService = inject(ApiService);
+
+  ngOnInit() {
+    this.cupcakes$ = this.getAllCupcakes();
+    console.log(this.cupcakes$);
+  }
+
+  getAllCupcakes() {
+   return this.apiService.getCupcakes()
+  }
 
   // Step 3: get all accessories
 
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Cupcake} from "../models/cupcake.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private apiUrl="http://localhost:4000/cupcakes";
+
+  private http: HttpClient = inject(HttpClient);
+
+  getCupcakes() {
+    return this.http.get<Cupcake>(`${this.apiUrl}`);
+  }
+
 }
