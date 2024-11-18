09646f89a21fbbb0914e38d9524b4d2408b35d03
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-11-18 10:52:13 +0100

    steps 1-5

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
+import { ApiService } from '../../shared/api.service';
+import { Cupcake } from '../../models/cupcake.model';
+import { Accessory } from '../../models/accesory.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cupcake-list',
   standalone: true,
-  imports: [CupcakeComponent],
+  imports: [CupcakeComponent, FormsModule],
   templateUrl: './cupcake-list.component.html',
   styleUrl: './cupcake-list.component.css',
 })
 export class CupcakeListComponent {
   // Step 1: get all cupcakes
-
+  private apiService = inject(ApiService);
+  allCupcakes!: Cupcake[];
   // Step 3: get all accessories
+  allAccessories!: Accessory[];
+  accessorySelected!: number | string;
 
+  // liste des cupcakes avec accessoire sélectionné
+  filteredCupcakes!: Cupcake[];
+  ngOnInit() {
+    this.apiService
+      .getCupcakes()
+      .subscribe((data) => (this.allCupcakes = data));
+
+    this.apiService
+      .getAccessories()
+      .subscribe((data) => (this.allAccessories = data));
+  }
+
+  filterCupcakes() {
+    this.filteredCupcakes = this.allCupcakes.filter(
+      (cupcake) => cupcake.accessory_id === this.accessorySelected
+    );
+    console.log(this.accessorySelected);
+  }
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Cupcake } from '../models/cupcake.model';
+import { Observable } from 'rxjs';
+import { Accessory } from '../models/accesory.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  getCupcakes(): Observable<Cupcake[]> {
+    return this.http.get<Cupcake[]>('http://localhost:4000/cupcakes');
+  }
+  getAccessories(): Observable<Accessory[]> {
+    return this.http.get<Accessory[]>('http://localhost:4000/accessories');
+  }
 }
