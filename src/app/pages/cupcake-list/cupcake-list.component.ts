94587d10617feba2e21fd860984cd85d9cb7ae22
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-11-18 11:19:04 +0100

    [STEP 4 & 5]: fill the accessories selector and bind it with FormsModule display the filter list of cupcakes

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -4,20 +4,23 @@ import { ApiService } from '../../shared/api.service';
 import { CupcakesList } from '../../models/cupcake.model';
 import { forkJoin, Observable, Subscription } from 'rxjs';
 import { AccessoriesList } from '../../models/accessorie.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cupcake-list',
   standalone: true,
-  imports: [CupcakeComponent],
+  imports: [CupcakeComponent, FormsModule],
   templateUrl: './cupcake-list.component.html',
   styleUrl: './cupcake-list.component.css',
 })
 export class CupcakeListComponent implements OnInit, OnDestroy {
   // Step 1: get all cupcakes
   public readonly cupcakesService = inject(ApiService);
-  public cupcakes!: CupcakesList;
+  private cupcakes!: CupcakesList;
+  public filteredCupcakes!: CupcakesList;
   public accessories!: AccessoriesList;
   private subscription!: Subscription;
+  public selectedAccessoryId: string = '';
 
   // Step 3: get all accessories
 
@@ -30,11 +33,20 @@ export class CupcakeListComponent implements OnInit, OnDestroy {
       accessories: this.cupcakesService.getAccessories(),
     }).subscribe(({ cupcakes, accessories }) => {
       this.cupcakes = cupcakes;
+      this.filteredCupcakes = [...this.cupcakes];
       this.accessories = accessories;
-      console.log('list de cupcakes', this.cupcakes);
-      console.log("list d'accessoires", this.accessories);
     });
+  }
 
+  //methods
+  getSelectedCupcakes(accessorieId: string): void {
+    if (accessorieId === '') {
+      this.filteredCupcakes = [...this.cupcakes];
+      return;
+    }
+    this.filteredCupcakes = this.cupcakes.filter(
+      (cupcake) => cupcake.accessory_id === String(this.selectedAccessoryId)
+    );
   }
 
   ngOnDestroy(): void {
