45d2d44c7277a0e9c78b495a3b0ac01f34bd1b8d
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-11-18 11:48:12 +0100

    fix: fix import of CommonModule for ngStyle

--- src/app/components/cupcake/cupcake.component.ts
+++ src/app/components/cupcake/cupcake.component.ts
@@ -1,11 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Cupcake } from '../../models/cupcake.model';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cupcake',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './cupcake.component.html',
   styleUrl: './cupcake.component.css',
 })
