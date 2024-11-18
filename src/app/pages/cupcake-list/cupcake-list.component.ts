51face1966146086b79c5c0e3c19d31128540cde
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-11-18 12:19:55 +0100

    Step 5 - Filter the list

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -2,31 +2,59 @@ import {Component, inject} from '@angular/core';
 import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
 import {Cupcake} from "../../models/cupcake.model";
 import {ApiService} from "../../shared/api.service";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {AsyncPipe, CommonModule} from "@angular/common";
+import {Accessory} from "../../models/accessory.model";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-cupcake-list',
   standalone: true,
-  imports: [CupcakeComponent, AsyncPipe, CommonModule],
+  imports: [CupcakeComponent, AsyncPipe, CommonModule, FormsModule],
   templateUrl: './cupcake-list.component.html',
   styleUrl: './cupcake-list.component.css',
 })
 export class CupcakeListComponent {
   // Step 1: get all cupcakes
   cupcakes$!: Observable<Cupcake[]>;
+  // Step 3: get all accessories
+  accessories$!: Observable<Accessory[]>;
+  accessoryId: number = 0
 
   private apiService: ApiService = inject(ApiService);
 
   ngOnInit() {
     this.cupcakes$ = this.getAllCupcakes();
-    console.log(this.cupcakes$);
+    this.accessories$ = this.getAllAccessories();
   }
 
   getAllCupcakes() : Observable<Cupcake[]> {
-   return this.apiService.getCupcakes()
+   return this.apiService.getCupcakes();
   }
 
-  // Step 3: get all accessories
+  getAllAccessories() : Observable<Accessory[]> {
+    return this.apiService.getAccessories();
+  }
 
+  /*onSelectChange(){
+    const target = event.target as HTMLSelectElement;
+    this.accessoryId = Number(target.value);
+    console.log('Selected accessory ID:', this.accessoryId);
+  }*/
+
+  onSelectChange() {
+    if (this.accessoryId !== 0) {
+      this.cupcakes$ = this.getAllCupcakes().pipe(
+        map(cupcakes => this.filterCupcakes(cupcakes))
+      );
+      console.log('Selected accessory ID:', this.accessoryId);
+      console.log('Cupcakes:', this.cupcakes$);
+    } else{
+      this.cupcakes$ = this.getAllCupcakes();
+    }
+  }
+
+  filterCupcakes(cupcakes: Cupcake[]): Cupcake[] {
+    return cupcakes.filter(cupcake => cupcake.accessory_id === this.accessoryId);
+  }
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,17 +1,21 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Cupcake} from "../models/cupcake.model";
+import {Accessory} from "../models/accessory.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl="http://localhost:4000/cupcakes";
-
   private http: HttpClient = inject(HttpClient);
 
   getCupcakes() {
-    return this.http.get<Cupcake[]>(`${this.apiUrl}`);
+    return this.http.get<Cupcake[]>("http://localhost:4000/cupcakes");
   }
+
+  getAccessories() {
+    return this.http.get<Accessory[]>(`http://localhost:4000/accessories`);
+  }
+
 }
