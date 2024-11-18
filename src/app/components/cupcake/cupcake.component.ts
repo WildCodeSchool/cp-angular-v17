21e8c29390ecbb267765ddcf8239159a16aa4e50
hannahazan <hannahazan2@gmail.com>
2024-11-18 10:39:13 +0100

    display cupcakes

--- src/app/components/cupcake/cupcake.component.ts
+++ src/app/components/cupcake/cupcake.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Cupcake } from '../../models/cupcake.model';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-cupcake',
@@ -9,12 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './cupcake.component.css'
 })
 export class CupcakeComponent {
-  cupcake: any = {
+  /*cupcake: Cupcake = {
     url: 'http://images.innoveduc.fr/php_parcours/cp2/donut.png',
     color1: 'var(--default-cream-color)',
     color2: 'var(--default-cream-color)',
     color3: 'var(--default-cream-color)',
     name: '',
-  };
+  };*/
+
+  @Input() cupcake: Cupcake = new Input();
 
 }
