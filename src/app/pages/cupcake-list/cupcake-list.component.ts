8eb48deca409262ba3deea357fbe613d7224257b
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-11-18 15:32:39 +0100

    fix: add error handler for cupcakes request, delete fallback request on console

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
 import { ApiService } from '../../shared/api.service';
 import { Cupcake } from '../../models/cupcake.model';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Accessories } from '../../models/accessories.model';
 import { FormsModule } from '@angular/forms';
@@ -19,11 +19,19 @@ export class CupcakeListComponent implements OnInit {
   apiService = inject(ApiService);
   cupcakes$!: Observable<Cupcake[]>;
   accessories$!: Observable<Accessories[]>;
+
   selectedAccessoryId: string = '';
   filteredCupcakes$!: Observable<Cupcake[]>;
+
+  error: string | null = null;
   // Step 1: get all cupcakes
   ngOnInit(): void {
-    this.cupcakes$ = this.apiService.getCupcakes();
+    this.cupcakes$ = this.apiService.getCupcakes().pipe(
+      catchError((err) => {
+        this.error = 'Failed to load cupcakes';
+        return of(err);
+      })
+    );
     // Step 3: get all accessories
     this.accessories$ = this.apiService.getAccessories();
 
