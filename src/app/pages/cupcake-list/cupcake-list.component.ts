2224144e683f48431b800a81dd5f9c25bcd545ad
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-11-18 10:10:05 +0100

    feat: get all cupCakes with an Input on app-cupcake component for a better reusability and maintainability

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
+import { ApiService } from '../../shared/api.service';
+import { Cupcake } from '../../models/cupcake.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cupcake-list',
   standalone: true,
-  imports: [CupcakeComponent],
+  imports: [CupcakeComponent, CommonModule],
   templateUrl: './cupcake-list.component.html',
   styleUrl: './cupcake-list.component.css',
 })
-export class CupcakeListComponent {
+export class CupcakeListComponent implements OnInit {
+  apiService = inject(ApiService);
+  cupcakes$!: Observable<Cupcake[]>;
   // Step 1: get all cupcakes
+  ngOnInit(): void {
+    this.cupcakes$ = this.apiService.getCupcakes();
+  }
 
   // Step 3: get all accessories
-
 }
