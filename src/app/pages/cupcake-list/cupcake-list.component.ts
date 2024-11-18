be1f81687e94be161d75fa62692035b858eccfce
Mudy Mud <bastien.masselon@laposte.net>
2024-11-18 10:59:34 +0100

    Step 3 - Get the accessories

--- src/app/pages/cupcake-list/cupcake-list.component.ts
+++ src/app/pages/cupcake-list/cupcake-list.component.ts
@@ -3,6 +3,7 @@ import { CupcakeComponent } from '../../components/cupcake/cupcake.component';
 import { Observable, Subscription } from 'rxjs';
 import { ApiService } from '../../shared/api.service';
 import { Cupcake } from '../../models/cupcake.model';
+import { Accessory } from '../../models/accessory.model';
 
 @Component({
   selector: 'app-cupcake-list',
@@ -12,24 +13,37 @@ import { Cupcake } from '../../models/cupcake.model';
   styleUrl: './cupcake-list.component.css',
 })
 export class CupcakeListComponent {
-  // Step 1: get all cupcakes
+  
   api : ApiService = inject(ApiService);
+  
   cupcakes! : Cupcake[];
+  accessories! : Accessory[];
   cupcakesSubscription! : Subscription;
+  accessoriesSubscription! : Subscription;
 
   ngOnInit() : void {
     this.cupcakesSubscription = this.getCupcakes().subscribe({
       next: response => this.cupcakes = response,
       error: error => console.log(error)
     });
+
+    this.accessoriesSubscription = this.getAccessories().subscribe({
+      next: response => this.accessories = response,
+      error: error => console.log(error)
+    });
   }
 
   ngOnDestroy() : void {
     this.cupcakesSubscription.unsubscribe();
+    this.accessoriesSubscription.unsubscribe();
   }
 
-  getCupcakes() : Observable<any> {
+  getCupcakes() : Observable<Cupcake[]> {
     return this.api.fetchCupcakes();
+  }
+
+  getAccessories() : Observable<Accessory[]> {
+    return this.api.fetchAccessories();
   }
 
   // Step 3: get all accessories
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cupcake } from '../models/cupcake.model';
+import { Accessory } from '../models/accessory.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ApiService {
 
   fetchCupcakes() : Observable<Cupcake[]>{
     return this.http.get<Cupcake[]>(this.apiUrl + '/cupcakes');
+  }
+
+  fetchAccessories() : Observable<Accessory[]> {
+    return this.http.get<Accessory[]>(this.apiUrl + '/accessories');
   }
 }
